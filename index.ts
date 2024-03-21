b55d7789cbeec8be216712964a9ce357dc058f37
Oluwaferanmi Ajiboye <atajiboyeo@gmail.com>
2024-03-21 14:26:02 +0100

    :ambulance: Added `Live` MongoDB URL

--- index.ts
+++ index.ts
@@ -28,7 +28,9 @@ app.use(
 app.use(bodyParser({ extended: true }));
 
 const PORT = 8080;
-const dbConnectString = "mongodb://127.0.0.1:27017/nafiam_cbt";
+// const dbConnectString = "mongodb://127.0.0.1:27017/nafiam_cbt";
+const dbConnectString =
+  "mongodb+srv://theajstars:XS4582oDGlvmhAub@data.hy4gux2.mongodb.net/?retryWrites=true&w=majority&appName=data";
 
 connect(dbConnectString)
   .then(() => {
